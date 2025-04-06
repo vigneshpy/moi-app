7eccaeb776d278ee4ec0b0d4e45083510446fb36
vigneshpy <vickypotter2516@gmail.com>
2025-04-06 23:15:30 +0530

    base url changes

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = process.env.API_URL || "http://192.168.0.103:3000";
+export const API_BASE_URL =
+	process.env.API_URL ||
+	"https://9of7ug7539.execute-api.ap-south-1.amazonaws.com";
 export const DEFAULT_COUNTRY_CODE = "+91";
 // "http://192.168.0.101:3000";
 // "http://moi-balancer-216064800.ap-south-1.elb.amazonaws.com";
